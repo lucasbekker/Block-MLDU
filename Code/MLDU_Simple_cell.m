282d16022c9c6685818b62cb728649c8ae7918b3
Lucas Bekker <lucasbekker@fastmail.nl>
2018-10-28 12:31:36 +0100

    A small rewrite with less dependency on nested functions and more code reuse.

--- Code/MLDU_Simple_cell.m
+++ Code/MLDU_Simple_cell.m
@@ -9,15 +9,18 @@ index = struct('M',i_j,'L',i_j,'D',i_j,'U',i_j);
 
 % Preallocate output matrices
 L_cell = cell(size(s));
-D_cell = cell(size(s));
-U_cell = cell(size(s));
+D_cell = L_cell; U_cell = L_cell;
 
 % Loop over the blocks
 for i = 1:s_l
    
-    update_index_nested(i);
+    j = update_index_nested(s(i),j,m,n);
     
-    fill_cells_nested(i);
+    L_cell{i} = fill_cells_local(A,L_cell{i},index.L.i,index.L.j);
+    D_cell{i} = fill_cells_local(A,D_cell{i},index.D.i,index.D.j);
+    U_cell{i} = fill_cells_local(A,U_cell{i},index.U.i,index.U.j);
+    
+    shift_index_nested(i,s_l);
     
     schur_complement =  A(index.L.i,index.L.j)* ...
                        (A(index.D.i,index.D.j)\ ...
@@ -27,12 +30,14 @@ for i = 1:s_l
            
 end
 
-collaps_cells();
+L = collaps_cell_local(L_cell,m,n);
+D = collaps_cell_local(D_cell,m,n);
+U = collaps_cell_local(U_cell,m,n);
 
-% Nested index function
-function [ ] = update_index_nested( i )
+% Nested index update function
+function [ j ] = update_index_nested( s, j, m, n )
    
-    index.D.i = (1:s(i)) + j;
+    index.D.i = (1:s) + j;
     index.D.j = index.D.i;
     index.L.i = (index.D.i(end) + 1):m;
     index.L.j = index.D.j;
@@ -41,28 +46,18 @@ function [ ] = update_index_nested( i )
     index.M.i = index.L.i;
     index.M.j = index.U.j;
     
-    j = s(i) + j;
+    j = s + j;
        
 end
 
-% Nested function that fills the cell arrays of L, D and U.
-function [ ] = fill_cells_nested( i )
-    
-    [L_cell{i}(1,:),L_cell{i}(2,:),L_cell{i}(3,:)] = ...
-        find(A(index.L.i,index.L.j));
-    
-    [D_cell{i}(1,:),D_cell{i}(2,:),D_cell{i}(3,:)] = ...
-        find(A(index.D.i,index.D.j));
-    
-    [U_cell{i}(1,:),U_cell{i}(2,:),U_cell{i}(3,:)] = ...
-        find(A(index.U.i,index.U.j));
-   
-    % Shift the index
+% Nested index shift function.
+function [ ] = shift_index_nested( i, s_end )
+
     D_cell{i}(1,:) = D_cell{i}(1,:) + (index.D.i(1) - 1);
     D_cell{i}(2,:) = D_cell{i}(2,:) + (index.D.j(1) - 1);
     
     % Only shift the index when not at the last itteration.
-    if i ~= s_l
+    if i ~= s_end
         
         L_cell{i}(1,:) = L_cell{i}(1,:) + (index.L.i(1) - 1);
         L_cell{i}(2,:) = L_cell{i}(2,:) + (index.L.j(1) - 1);
@@ -70,20 +65,26 @@ function [ ] = fill_cells_nested( i )
         U_cell{i}(2,:) = U_cell{i}(2,:) + (index.U.j(1) - 1);
         
     end
-    
+
 end
 
-% Nested function that converts the cell arrays to regular spare matrices.
-function [ ] = collaps_cells( )
-    
-    L_col = [L_cell{:}];
-    D_col = [D_cell{:}];
-    U_col = [U_cell{:}];
-
-    L = sparse(L_col(1,:),L_col(2,:),L_col(3,:),m,n);
-    D = sparse(D_col(1,:),D_col(2,:),D_col(3,:));
-    U = sparse(U_col(1,:),U_col(2,:),U_col(3,:),m,n);
-    
 end
 
+% Local function that fills the cell array.
+function [ A_cell ] = fill_cells_local( A, A_cell, index_i, index_j )
+    
+    % Insert COO representation of selected A entries.
+    [A_cell(1,:),A_cell(2,:),A_cell(3,:)] = find(A(index_i,index_j));
+
+end
+
+% Local function that converts the cell array to a regular sparse matrices.
+function [ A ] = collaps_cell_local( A_cell, m, n )
+    
+    % Strange matlab syntax that just works in this particalur case...
+    A_col = [A_cell{:}];
+    
+    % Convert COO to CCS (MATLAB) sparse.
+    A = sparse(A_col(1,:),A_col(2,:),A_col(3,:),m,n);
+    
 end
